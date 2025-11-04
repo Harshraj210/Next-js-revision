186449f12b7e6eb7755e3279bdabfad44d862d65
harsh <nh1750501@gmail.com>
2025-11-04 21:01:32 +0530

    Added dynamic routing

---
+++ 01_hello_world/app/about/page.js
@@ -0,0 +1,11 @@
+import AboutContent from '../../components/AboutContent'; // Import your component
+
+// This is the "About" page!
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>Welcome to the About Page</h1>
+      <AboutContent /> 
+    </div>
+  );
+}
---
+++ 01_hello_world/app/contact/page.js
@@ -0,0 +1,12 @@
+import ContactForm from '../../components/ContactForm.js'; 
+
+// This is the "Contact" page!
+export default function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>Please fill out the form below.</p>
+      <ContactForm /> 
+    </div>
+  );
+}
